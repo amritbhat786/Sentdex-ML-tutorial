3154c102039d9574ac4dfa031d3f12920fa71fa1
amrit.bhat786 <amrit.bhat786@gmail.com>
2017-06-25 13:13:56 +0530

    Created linear regression dir

--- linear-regression/ML2.py
+++ linear-regression/ML2.py
@@ -5,10 +5,10 @@ df =  quandl.get('WIKI/GOOGL')
 
 #print(df.head())
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
-high = df.loc['Adj. High']
-close = df.loc['Adj. Close']
-Open = df.loc['Adj. Open'] 
-print( Open)
+high = df['Adj. High']
+close = df['Adj. Close']
+Open = df['Adj. Open'] 
+print( df )
 df['hl_pct'] = {high - close} / close 
 df['pct_change'] = {close - Open} / Open 
 
